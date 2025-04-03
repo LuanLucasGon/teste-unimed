b78d8810b82ce31b099eae4cf0f04c34046e067d
David <d202410759@uftm.edu.br>
2025-04-03 13:51:41 -0300

    feat: creating files for project

---
+++ unimed-back/src/main/java/com/example/unimed_back/UnimedBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.unimed_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimedBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimedBackApplication.class, args);
+	}
+
+}
---
+++ unimed-back/src/test/java/com/example/unimed_back/UnimedBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unimed_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnimedBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
